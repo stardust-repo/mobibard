995b0fd237b10deb53798f7a28c2eef50bccad44
raccoon <leafpaw.raccoon@gmail.com>
2026-06-28 02:18:53 +0900

    키설정

--- player/js/google-config.js
+++ player/js/google-config.js
@@ -1,9 +1,5 @@
-// Google Drive 연동 설정
-// Google Cloud Console에서 Web application OAuth Client ID와 API Key를 발급받아 넣어주세요.
-// 로컬 테스트 origin 예: http://localhost:8000
 window.MOBIBARD_GOOGLE_CONFIG = {
-  clientId: "",
-  apiKey: "",
-  // Google Picker에서 사용할 Google Cloud 프로젝트 번호입니다. 비워도 저장 기능은 동작합니다.
-  appId: ""
+  clientId: "611500975884-mcv2g3te9i8dr7u41kt279i5qs9b9jth.apps.googleusercontent.com",
+  apiKey: "AIzaSyAa2ZtTpBR0Q0UYS7WfYkV2ReevaK50LXI",
+  appId: "611500975884"
 };
